f417595e68674272fb74247e0404f3d599bfd6aa
Emmanuel Ablao <149139327+Lazytech15@users.noreply.github.com>
2024-05-24 18:06:18 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,83 @@
+
+function isViewportLessThan700px() {
+    return window.innerWidth < 700;
+}
+
+function scrollFunction() {
+    if (isViewportLessThan700px()) return;
+
+    const navigationButton = document.getElementsByClassName('image-nav-bar-container')[0];
+    const navigationContent = document.getElementsByClassName('image-nav-bar-content')[0];
+    const header_log = document.getElementById('header-nav');
+    const navBarRect = navigationButton.getBoundingClientRect();
+    const windowHeight = window.innerHeight || document.documentElement.clientHeight;
+
+    if (!(navBarRect.top >= 0 && navBarRect.bottom <= windowHeight)) {
+        navigationContent.classList.add('sticky-nav');
+        header_log.style.display="flex";
+    } else {
+        navigationContent.classList.remove('sticky-nav');
+        header_log.style.display="none";
+    }
+}
+
+if (!isViewportLessThan700px()) {
+    window.onscroll = scrollFunction;
+}
+
+function isViewportLessThanOrEqualTo700px() {
+    return window.innerWidth <= 700;
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+    const showMoreButton = document.getElementById('showMoreButton');
+    const hiddenImages = document.querySelectorAll('.gallery-content img:nth-child(n+4)');
+    const serviceContent = document.getElementById('serviceContent');
+    let isHidden = true;
+
+    function toggleHiddenImages() {
+        hiddenImages.forEach(img => {
+            img.style.display = isHidden ? 'block' : 'none';
+        });
+
+        if (isHidden) {
+            if (isViewportLessThanOrEqualTo700px()) {
+                const gallerySection = document.querySelector('.gallery-section');
+                let galleryHeight = gallerySection.scrollHeight;
+                galleryHeight = galleryHeight > 600 ? 600 : galleryHeight;
+                serviceContent.style.marginTop = `${galleryHeight}px`;
+            }
+        } else {
+            serviceContent.style.marginTop = '0';
+        }
+
+        showMoreButton.textContent = isHidden ? 'Hide' : 'Show More';
+        isHidden = !isHidden;
+    }
+
+    showMoreButton.addEventListener('click', function() {
+        toggleHiddenImages();
+    });
+
+    // Set initial image visibility based on viewport width
+    if (isViewportLessThanOrEqualTo700px()) {
+        hiddenImages.forEach(img => {
+            img.style.display = 'none';
+        });
+    } else {
+        hiddenImages.forEach(img => {
+            img.style.display = 'block';
+        });
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
